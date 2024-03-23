47514f0f3c4e2d33fac5c1145de67a03c944e165
jdfnc24 <59299498+jdfnc24@users.noreply.github.com>
2024-03-23 15:40:54 -0500

    Initial commit

---
+++ ModTemplate/Scripts/config.cpp
@@ -0,0 +1,64 @@
+class CfgPatches
+{
+	class MT_Scripts
+	{
+		requiredAddons[] = { "DZ_Scripts" };
+	};
+};
+
+class CfgAddons
+{
+    class PreloadAddons
+    {
+        class ModTemplate
+        {
+            list[]={};
+        };
+    };
+};
+
+class CfgMods
+{
+    class ModTemplate
+    {
+        name="";
+        dir="ModTemplate";
+        picture="";
+        action="";
+        author="";
+        overview = "";
+		inputs = "";
+        defines[] = {};
+
+        class defs
+		{
+			class imageSets
+			{
+				files[]= {};
+			};
+			class widgetStyles
+			{
+				files[]= {};
+			};
+
+			class engineScriptModule 
+			{ 
+				files[] = { "ModTemplate/Scripts/1_Core"};
+			};
+
+			class gameScriptModule
+			{
+				files[] = { "ModTemplate/Scripts/3_Game" };
+			};
+			class worldScriptModule
+			{
+				files[] = { "ModTemplate/Scripts/4_World" };
+			};
+
+			class missionScriptModule 
+			{
+				files[] = { "ModTemplate/Scripts/5_Mission" };
+			};
+		};
+    };
+};
